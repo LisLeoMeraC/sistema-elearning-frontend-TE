040012e7acc522ed037b552258e1d14abf9258cd
LisLeoMeraC <meraleonardo99@gmail.com>
2024-03-11 17:53:55 -0500

    updateFiles

--- src/app/recurso-service.service.ts
+++ src/app/recurso-service.service.ts
@@ -1,14 +1,16 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import baseURL from './services/helper';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecursoServiceService {
 
-  private baseUrl = 'http://localhost:8080/archivos';
-  private viewRecursos= 'http://localhost:8080/archivos/categoria'
+ 
 
   constructor(private http: HttpClient) { }
 
@@ -17,10 +19,10 @@ export class RecursoServiceService {
     formData.append('file', archivo, archivo.name);
     formData.append('categoriaId', categoriaId.toString());
 
-    return this.http.post(`${this.baseUrl}/subir`, formData, { responseType: 'text' });
+    return this.http.post(`${baseURL}/archivos/subir`, formData, { responseType: 'text' });
   }
 
   getArchivosPorCategoria(idCategoria: number): Observable<any> {
-    return this.http.get(`${this.viewRecursos}/${idCategoria}`);
+    return this.http.get(`${baseURL}/archivos/categoria/${idCategoria}`);
   }
 }
